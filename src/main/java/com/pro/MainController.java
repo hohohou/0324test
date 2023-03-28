32e9e4365088939198ba4e0fb57272e06dc65f31
hohohou <minho0327@naver.com>
2023-03-28 17:35:19 +0900

    0328 1735

--- src/main/java/com/pro/MainController.java
+++ src/main/java/com/pro/MainController.java
@@ -9,5 +9,25 @@ public class MainController {
 	public String main() {
 		return "main";
 	}
+	@RequestMapping("/search/tag/view")
+	public String searchTagView() {
+		
+		return "";
+	}
+	@RequestMapping("/week/content/view")
+	public String weekContentView() {
+		
+		return "day_content";
+	}
+	@RequestMapping("/recommend/theme/view")
+	public String recommentThemeView() {
+		
+		return "Member_Theme_Recommendation";
+	}
+	@RequestMapping("/membership/view")
+	public String membershipView() {
+		
+		return "";
+	}
 
 }
